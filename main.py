046b29fa04c8b4011182b9e662bc21868b3a54c2
Atul Singh <asatulsingh20@gmail.com>
2021-01-04 21:52:57 +0530

    Update

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+from UI.MainWindow import Ui_MainWindow
+
+class UI(Ui_MainWindow):
+    def populateTypeOfAlgo(self):
+        pass
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = UI()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
