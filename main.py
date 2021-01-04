d0961cab06cb14fe17c86d1f9d356b39c3716bdf
Atul Singh <asatulsingh20@gmail.com>
2021-01-04 22:30:39 +0530

    Populated Algorithms

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ class UI(Ui_MainWindow):
     def populateTypeOfAlgo(self):
         pass
 
-    def populateAlgo(self):
-        pass
+    def addItemToAlgoBox(self, items):
+        self.AlgoBox.clear()
+        items.sort()
+        for item in items:
+                self.AlgoBox.addItem(item)
+
+    def populateAlgo(self, nameOfAlgo):
+        if nameOfAlgo is "Searching":
+            items = ["Linear Searching", "Binary Searching"]
+            self.addItemToAlgoBox(items)
+
+        if nameOfAlgo is "Sorting":
+            items = ["Selection Sort", "Bubble Sort", "Merge Sort", "Quick Sort", "Heap Sort", "Insertion Sort"]
+            self.addItemToAlgoBox(items)
+
+        if nameOfAlgo is "Arrays":
+            pass
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = UI()
 ui.setupUi(MainWindow)
+ui.populateAlgo("Searching")
 MainWindow.show()
 sys.exit(app.exec_())
